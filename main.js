e3a9067d82770dcfa1c4e13348a15c78137c459f
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 17:01:04 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//set value in span using jquery
+$("#idname").text("your value");
+
+//set value in span using Javascript
+document.getElementById("idname").textContent="Your value";
+document.getElementById("message").innerHTML="your value";
